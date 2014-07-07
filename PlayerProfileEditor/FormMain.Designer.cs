0d6a2f4cd53f9ba5ea47fc566efc2367ebccbb55
Angelo Geels <spansjh@gmail.com>
2014-07-07 12:58:52 +0200

    Added sources

---
+++ PlayerProfileEditor/FormMain.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace PlayerProfileEditor
+{
+  partial class FormMain
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null)) {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+      this.textName = new System.Windows.Forms.TextBox();
+      this.label1 = new System.Windows.Forms.Label();
+      this.label2 = new System.Windows.Forms.Label();
+      this.textGUID = new System.Windows.Forms.TextBox();
+      this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+      this.openToolStripButton = new System.Windows.Forms.ToolStripButton();
+      this.saveToolStripButton = new System.Windows.Forms.ToolStripButton();
+      this.textTeam = new System.Windows.Forms.TextBox();
+      this.label3 = new System.Windows.Forms.Label();
+      this.picName = new System.Windows.Forms.PictureBox();
+      this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+      this.colorToolStripButton = new System.Windows.Forms.ToolStripButton();
+      this.gradientToolStripButton = new System.Windows.Forms.ToolStripButton();
+      this.toolStrip1.SuspendLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.picName)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // textName
+      // 
+      this.textName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                  | System.Windows.Forms.AnchorStyles.Right)));
+      this.textName.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.textName.Location = new System.Drawing.Point(54, 67);
+      this.textName.Name = "textName";
+      this.textName.Size = new System.Drawing.Size(527, 20);
+      this.textName.TabIndex = 0;
+      this.textName.TextChanged += new System.EventHandler(this.textName_TextChanged);
+      // 
+      // label1
+      // 
+      this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(10, 70);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(38, 13);
+      this.label1.TabIndex = 1;
+      this.label1.Text = "Name:";
+      // 
+      // label2
+      // 
+      this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(11, 122);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(37, 13);
+      this.label2.TabIndex = 2;
+      this.label2.Text = "GUID:";
+      // 
+      // textGUID
+      // 
+      this.textGUID.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      this.textGUID.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.textGUID.Location = new System.Drawing.Point(54, 119);
+      this.textGUID.Name = "textGUID";
+      this.textGUID.Size = new System.Drawing.Size(244, 20);
+      this.textGUID.TabIndex = 3;
+      // 
+      // toolStrip1
+      // 
+      this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+      this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripButton,
+            this.saveToolStripButton,
+            this.toolStripSeparator1,
+            this.colorToolStripButton,
+            this.gradientToolStripButton});
+      this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+      this.toolStrip1.Name = "toolStrip1";
+      this.toolStrip1.Size = new System.Drawing.Size(593, 25);
+      this.toolStrip1.TabIndex = 4;
+      this.toolStrip1.Text = "toolStrip1";
+      // 
+      // openToolStripButton
+      // 
+      this.openToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+      this.openToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("openToolStripButton.Image")));
+      this.openToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+      this.openToolStripButton.Name = "openToolStripButton";
+      this.openToolStripButton.Size = new System.Drawing.Size(23, 22);
+      this.openToolStripButton.Text = "&Open";
+      this.openToolStripButton.Click += new System.EventHandler(this.openToolStripButton_Click);
+      // 
+      // saveToolStripButton
+      // 
+      this.saveToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+      this.saveToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("saveToolStripButton.Image")));
+      this.saveToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+      this.saveToolStripButton.Name = "saveToolStripButton";
+      this.saveToolStripButton.Size = new System.Drawing.Size(23, 22);
+      this.saveToolStripButton.Text = "&Save";
+      this.saveToolStripButton.Click += new System.EventHandler(this.saveToolStripButton_Click);
+      // 
+      // textTeam
+      // 
+      this.textTeam.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                  | System.Windows.Forms.AnchorStyles.Right)));
+      this.textTeam.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.textTeam.Location = new System.Drawing.Point(54, 93);
+      this.textTeam.Name = "textTeam";
+      this.textTeam.Size = new System.Drawing.Size(527, 20);
+      this.textTeam.TabIndex = 5;
+      this.textTeam.TextChanged += new System.EventHandler(this.textTeam_TextChanged);
+      // 
+      // label3
+      // 
+      this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      this.label3.AutoSize = true;
+      this.label3.Location = new System.Drawing.Point(11, 96);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(37, 13);
+      this.label3.TabIndex = 1;
+      this.label3.Text = "Team:";
+      // 
+      // picName
+      // 
+      this.picName.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                  | System.Windows.Forms.AnchorStyles.Left)
+                  | System.Windows.Forms.AnchorStyles.Right)));
+      this.picName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+      this.picName.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+      this.picName.Location = new System.Drawing.Point(12, 28);
+      this.picName.Name = "picName";
+      this.picName.Size = new System.Drawing.Size(569, 33);
+      this.picName.TabIndex = 6;
+      this.picName.TabStop = false;
+      // 
+      // toolStripSeparator1
+      // 
+      this.toolStripSeparator1.Name = "toolStripSeparator1";
+      this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+      // 
+      // colorToolStripButton
+      // 
+      this.colorToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+      this.colorToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("colorToolStripButton.Image")));
+      this.colorToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+      this.colorToolStripButton.Name = "colorToolStripButton";
+      this.colorToolStripButton.Size = new System.Drawing.Size(23, 22);
+      this.colorToolStripButton.Text = "Color";
+      this.colorToolStripButton.Click += new System.EventHandler(this.colorToolStripButton_Click);
+      // 
+      // gradientToolStripButton
+      // 
+      this.gradientToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+      this.gradientToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("gradientToolStripButton.Image")));
+      this.gradientToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+      this.gradientToolStripButton.Name = "gradientToolStripButton";
+      this.gradientToolStripButton.Size = new System.Drawing.Size(23, 22);
+      this.gradientToolStripButton.Text = "Gradient";
+      this.gradientToolStripButton.Click += new System.EventHandler(this.gradientToolStripButton_Click);
+      // 
+      // FormMain
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(593, 151);
+      this.Controls.Add(this.picName);
+      this.Controls.Add(this.textTeam);
+      this.Controls.Add(this.toolStrip1);
+      this.Controls.Add(this.textGUID);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.label3);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.textName);
+      this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+      this.Name = "FormMain";
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+      this.Text = "Player profile editor";
+      this.toolStrip1.ResumeLayout(false);
+      this.toolStrip1.PerformLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.picName)).EndInit();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.TextBox textName;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.TextBox textGUID;
+    private System.Windows.Forms.ToolStrip toolStrip1;
+    private System.Windows.Forms.ToolStripButton openToolStripButton;
+    private System.Windows.Forms.ToolStripButton saveToolStripButton;
+    private System.Windows.Forms.TextBox textTeam;
+    private System.Windows.Forms.Label label3;
+    private System.Windows.Forms.PictureBox picName;
+    private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+    private System.Windows.Forms.ToolStripButton colorToolStripButton;
+    private System.Windows.Forms.ToolStripButton gradientToolStripButton;
+  }
+}
+
---
+++ PlayerProfileEditor/FormMain.cs
@@ -0,0 +1,315 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Globalization;
+
+namespace PlayerProfileEditor
+{
+  public partial class FormMain : Form
+  {
+    string fnmProfile;
+    byte[] abAppearance;
+
+    public FormMain(string[] args)
+    {
+      InitializeComponent();
+
+      if (args.Length > 0) {
+        fnmProfile = args[0];
+        LoadProfile();
+      }
+    }
+
+    private void openToolStripButton_Click(object sender, EventArgs e)
+    {
+      OpenFileDialog ofd = new OpenFileDialog();
+      ofd.Filter = "Player files (*.plr)|*.plr|All files (*.*)|*.*";
+      if (ofd.ShowDialog() == DialogResult.OK) {
+        fnmProfile = ofd.FileName;
+        LoadProfile();
+      }
+    }
+
+    private void saveToolStripButton_Click(object sender, EventArgs e)
+    {
+      SaveProfile();
+    }
+
+    void PictureText(string strText)
+    {
+      Bitmap bmp = new Bitmap(picName.Width, picName.Height);
+      Graphics g = Graphics.FromImage(bmp);
+
+      g.Clear(picName.BackColor);
+      g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+      float fXPos = 0;
+      bool bBold = false;
+      bool bItalic = false;
+      Color colColor = Color.White;
+
+      for (int i = 0; i < strText.Length; ) {
+        if (strText[i] == '^') {
+          if (i == strText.Length - 1) {
+            break;
+          }
+          switch (strText[++i]) {
+            case 'r':
+              bBold = false;
+              bItalic = false;
+              colColor = Color.White;
+              break;
+
+            case 'c':
+              i++;
+              try {
+                byte ubRed = byte.Parse(strText.Substring(i, 2), NumberStyles.AllowHexSpecifier); i += 2;
+                byte ubGreen = byte.Parse(strText.Substring(i, 2), NumberStyles.AllowHexSpecifier); i += 2;
+                byte ubBlue = byte.Parse(strText.Substring(i, 2), NumberStyles.AllowHexSpecifier); i++;
+                colColor = Color.FromArgb(255, ubRed, ubGreen, ubBlue);
+              } catch {
+                colColor = Color.Black;
+              }
+              break;
+            case 'b': bBold = true; break;
+            case 'i': bItalic = true; break;
+            case 'f': i++; break;
+
+            case 'C': colColor = Color.White; break;
+            case 'B': bBold = false; break;
+            case 'I': bItalic = false; break;
+
+            case '^':
+              Font fnt = new Font(SystemFonts.DefaultFont, FontStyle.Regular | (bBold ? FontStyle.Bold : 0) | (bItalic ? FontStyle.Italic : 0));
+              g.DrawString("^", fnt, new SolidBrush(colColor), new PointF(fXPos, 4));
+              fXPos += g.MeasureString("^", fnt).Width * 0.8f;
+              i++;
+              break;
+          }
+          i++;
+        } else {
+          if (i < strText.Length) {
+            string strChar = new string(strText[i], 1);
+            Font fnt = new Font(SystemFonts.DefaultFont, FontStyle.Regular | (bBold ? FontStyle.Bold : 0) | (bItalic ? FontStyle.Italic : 0));
+            g.DrawString(strChar, fnt, new SolidBrush(colColor), new PointF(fXPos, 4));
+            fXPos += g.MeasureString(strChar, fnt).Width * 0.8f;
+            i++;
+          }
+        }
+      }
+
+      picName.Image = bmp;
+    }
+
+    string RemoveCodes(string str)
+    {
+      string str_ret = "";
+      for (int i = 0; i < str.Length; i++) {
+        if (str[i] == '^') {
+          char c = str[++i];
+          switch (c) {
+            case '^': str_ret += '^'; break;
+            case 'c': i += 6; break;
+            case 'a': i += 2; break;
+            case 'f': i++; break;
+          }
+        } else {
+          str_ret += str[i];
+        }
+      }
+      return str_ret;
+    }
+
+    void LoadProfile()
+    {
+      using (BinaryReader reader = new BinaryReader(File.OpenRead(fnmProfile))) {
+        if (reader.ReadInt32() != 876825680 /* PLC4 */) {
+          MessageBox.Show("Header not found!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+          return;
+        }
+
+        string strName = "";
+        string strTeam = "";
+
+        {
+          int iLen = reader.ReadInt32();
+          strName = Encoding.ASCII.GetString(reader.ReadBytes(iLen));
+          textName.Text = strName;
+        }
+
+        {
+          int iLen = reader.ReadInt32();
+          strTeam = Encoding.ASCII.GetString(reader.ReadBytes(iLen));
+          textTeam.Text = strTeam;
+        }
+
+        byte[] abGUID = reader.ReadBytes(16);
+        abAppearance = reader.ReadBytes(32);
+
+        string strGUID = "";
+        for (int i = 0; i < 16; i++) {
+          strGUID += abGUID[i].ToString("x2");
+        }
+        textGUID.Text = strGUID.ToUpper();
+
+        Text = "Player profile editor - " + fnmProfile;
+      }
+    }
+
+    void SaveProfile()
+    {
+      if (textGUID.Text.Length != 32) {
+        MessageBox.Show("Invalid GUID!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        return;
+      }
+
+      for (int i = 0; i < 32; i++) {
+        if (!"0123456789abcdef".Contains(textGUID.Text.ToLower()[i])) {
+          MessageBox.Show("Invalid GUID!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+          return;
+        }
+      }
+
+      if (File.Exists(fnmProfile)) {
+        File.Delete(fnmProfile);
+      }
+
+      using (BinaryWriter writer = new BinaryWriter(File.OpenWrite(fnmProfile))) {
+        writer.Write(Encoding.ASCII.GetBytes("PLC4"));
+
+        writer.Write(textName.Text.Length);
+        writer.Write(Encoding.ASCII.GetBytes(textName.Text));
+
+        writer.Write(textTeam.Text.Length);
+        writer.Write(Encoding.ASCII.GetBytes(textTeam.Text));
+
+        byte[] abGUID = new byte[16];
+        for (int i = 0; i < 32; i += 2) {
+          string strPart = textGUID.Text.Substring(i, 2);
+          abGUID[i / 2] = byte.Parse(strPart, NumberStyles.AllowHexSpecifier);
+        }
+
+        writer.Write(abGUID);
+        writer.Write(abAppearance);
+      }
+    }
+
+    private void textName_TextChanged(object sender, EventArgs e)
+    {
+      PictureText(textName.Text);
+    }
+
+    private void textTeam_TextChanged(object sender, EventArgs e)
+    {
+      PictureText(textTeam.Text);
+    }
+
+    Color GetPureColorCodeFromDialog()
+    {
+      ColorDialog dialog = new ColorDialog();
+      if (dialog.ShowDialog() != DialogResult.OK) {
+        return Color.Transparent;
+      }
+      return dialog.Color;
+    }
+
+    string ColorCodeFromColor(Color col)
+    {
+      return col.R.ToString("x2") + col.G.ToString("x2") + col.B.ToString("x2");
+    }
+
+    string GetColorCodeFromDialog()
+    {
+      Color col = GetPureColorCodeFromDialog();
+      if (col == Color.Transparent) {
+        return "";
+      }
+      return ColorCodeFromColor(col);
+    }
+
+    private void colorToolStripButton_Click(object sender, EventArgs e)
+    {
+      int sel_start = textName.SelectionStart;
+      int sel_len = textName.SelectionLength;
+      string str = textName.Text;
+
+      string col = GetColorCodeFromDialog();
+      if (col == "") {
+        return;
+      }
+
+      str = str.Insert(sel_start, "^c" + col);
+      if (sel_len > 0) {
+        str = str.Insert(sel_start + 8 + sel_len, "^C");
+      }
+
+      textName.Text = str;
+      textName.Focus();
+      textName.SelectionStart = sel_start;
+      textName.SelectionLength = sel_len + 8 + 2; // ^cff0000 (8) + ^C (2)
+    }
+
+    float Lerp(float val1, float val2, float x)
+    {
+      return val1 + (val2 - val1) * x;
+    }
+
+    Color Lerp(Color col1, Color col2, float x)
+    {
+      int r = (int)Lerp((float)col1.R, (float)col2.R, x);
+      int g = (int)Lerp((float)col1.G, (float)col2.G, x);
+      int b = (int)Lerp((float)col1.B, (float)col2.B, x);
+      return Color.FromArgb(r, g, b);
+    }
+
+    private void gradientToolStripButton_Click(object sender, EventArgs e)
+    {
+      int sel_start = textName.SelectionStart;
+      int sel_len = textName.SelectionLength;
+      string str = textName.Text;
+
+      if (sel_start == str.Length) {
+        sel_start = 0;
+      }
+      if (sel_len == 0) {
+        sel_len = str.Length - sel_start;
+      }
+
+      Color col1 = GetPureColorCodeFromDialog();
+      if (col1 == Color.Transparent) {
+        return;
+      }
+
+      Color col2 = GetPureColorCodeFromDialog();
+      if (col2 == Color.Transparent) {
+        return;
+      }
+
+      string str_add = "";
+      for (int i = 0; i < sel_len; i++) {
+        if (str[sel_start + i] == '^') {
+          char c = str[sel_start + ++i];
+          if (c != '^') {
+            switch (c) {
+              case 'c': i += 6; break;
+              case 'f': str_add += "^f" + str[sel_start + ++i]; break;
+              case 'a': str_add += "^a" + str.Substring(sel_start + ++i, 2); i += 2; break;
+            }
+            continue;
+          }
+        }
+        Color col = Lerp(col1, col2, (float)i / (float)sel_len);
+        str_add += "^c" + ColorCodeFromColor(col) + str[sel_start + i];
+      }
+      str = str.Substring(0, sel_start) + str_add + "^C" + str.Substring(sel_start + sel_len);
+
+      textName.Text = str;
+    }
+  }
+}
